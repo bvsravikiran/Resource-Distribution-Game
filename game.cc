6153964cdce8f69f8386d700bdfcb4df45d20b30
Ravi <vsravikiran.b@gmail.com>
2018-05-07 16:48:49 -0500

    calculated the shapley's value.

--- game.cc
+++ game.cc
@@ -14,13 +14,11 @@ private:
 	vector<int> res;
 	int total_res;
 	vector<int> util;
-	long fact;
 public:
 	Game(int n) {
 		agents = n;
 		init_();
 		get_resources();
-		cout << "Hello!" << endl;
 	}
 
 	void get_resources() {
@@ -46,44 +44,27 @@ public:
 		} while(next_permutation(init.begin(), init.end()));
 	}
 
-	unsigned long factorial(unsigned int n) {
-		if (n == 0) return 1;
-		return n * factorial(n-1);
-	}
+	unsigned long factorial(unsigned int n) { return (n == 0) ? 1 : n * factorial(n-1); }
 
-	bool is_greater(int n) {
-		return ((n/total_res)*100 > 50) ? true : false;
-	}
+	bool is_greater(int n) { return (n > 50) ? true : false; }
 
 	void util_() {
-		fact = factorial(agents);
-		//cout << "util_ function called" << endl;
 		util.reserve(agents);
-		for(int i = 0; i < 1; ++i) {
-			//util[i] = shapley(i+1);
-			cout << shapley(i+1) << endl;
+		for(int i = 0; i < agents; ++i) {
+			cout << "utility for the agent " << i + 1 << " is " << shapley(i + 1) << endl;
 		}
-		cout << "Success!" << endl;
 	}
 
 	double shapley(int n) {
-		//cout << "shapley function called for agent " << n << endl;
 		int sum = 0;
 		double value = 0;
 		int counter = 0;
 		for (int i = 0; i < perm.size(); ++i) {
-			cout << "For permutation ";
-			for (int k = 0; k < perm[i].size(); ++k) {
-				cout << perm[i][k] << " ";
-			}
-			cout << endl;
 			for(int j = 0; j < perm[i].size(); ++j) {
 				if (perm[i][j] == n) {
 					if (!is_greater(sum)) {
 						if (is_greater(sum + res[n])) {
-							cout << counter << "! " << agents - counter - 1 << "! " << "(100 - 0)";
 							double temp = (factorial(counter) * factorial(agents - counter - 1) * 100);
-							cout << temp << endl;
 							value += temp;
 						}
 					}
@@ -91,14 +72,12 @@ public:
 					counter = 0;
 					break;
 				} else {
-					//cout << "agent " << n << " not yet encountered" << endl;
 					sum += res[perm[i][j]];
-					//cout << "Sum: " << sum;
 					counter++;
 				}
 			}
 		}
-		return value /factorial(fact);
+		return value / factorial(agents);
 	}
 
 };
